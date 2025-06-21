a51fedd0ca818ec62e54f3cc82f61efb3cbe6eb5
MoiOrMe <robin.rivard@efrei.net>
2025-06-21 13:09:44 +0200

    Update TP2.py

--- TP2.py
+++ TP2.py
@@ -1,5 +1,5 @@
 def Exercice_1():
-    print("Exercice 1 : Work in progress")
+    print("Exercice : Work in progress")
     # Placeholder for future implementation
     pass
 
@@ -7,7 +7,7 @@ def ChoixExoTP2():
     choiceExo = -1
     print("Bienvenue dans le TP de Reconnaissance de Visages !")
     while choiceExo != 0:
-        choiceExo = int(input("Quelle exercice voulez-vous lancer ? :\n1. Exercice 1 (Work in progress)\n0. Quitter\n"))
+        choiceExo = int(input("Quelle exercice voulez-vous lancer ? :\n1. Exercice (Work in progress)\n0. Quitter\n"))
         if choiceExo == 1:
             Exercice_1()
         elif choiceExo < 0 or choiceExo > 1:
